7fc71278206883528177031dc507c1cf6bfad956
NandishM0618 <nandish1729@gmail.com>
2025-10-30 19:14:45 +0530

    chore(frontend): update frontend success page

--- src/frontend/app/success/page.tsx
+++ src/frontend/app/success/page.tsx
@@ -1,30 +1,10 @@
-'use client'
-import Image from "next/image";
-import Link from "next/link";
-import success from '@/public/assets/ep_success-filled.png'
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import SuccessPageContent from "./SuccessPageContext";
 
-export default function Success() {
-    const searchParams = useSearchParams();
-
-    const id = searchParams.get("booking");
-
+export default function SuccessPage() {
     return (
-        <div className="flex flex-col items-center py-20">
-            <Image
-                src={success}
-                alt="success"
-                width={100}
-                height={100}
-                className="object-contain"
-            />
-            <h2 className="text-3xl font-bold text-green-600"> Booking Successful!</h2>
-            <p className="mt-2 text-gray-600">Ref ID: {id}</p>
-            <div className="bg-gray-300 rounded-sm mt-8 p-2">
-                <Link href="/" className="mt-6 text-sm  text-black">
-                    Back to home
-                </Link>
-            </div>
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <SuccessPageContent />
+        </Suspense>
     );
 }
